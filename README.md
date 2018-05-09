7622d64da9bec8443570498cbbaa8a71bd139e82
Yixiong <yixiong@kesci.com>
2018-05-09 17:51:19 +0800

    update qichedashi

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# qichedashi_evaluation
+# PaddlePaddle AI 产业应用赛——汽车大师问答摘要与推理 --- 评审环节
+
+欢迎参加[科赛网](https://www.kesci.com)和百度联合举办的[PaddlePaddle AI 产业应用赛——汽车大师问答摘要与推理](https://www.kesci.com/apps/home/competition/5aec0eb10739c42faa203931)。我们提供此次比赛的评审脚本，供选手在K-Lab内对个人的模型进行 **验证(validation)**。
+
+## 算法介绍
+
+评测用到的核心算法为[ROUGE](https://en.wikipedia.org/wiki/ROUGE_(metric))，即：Recall-Oriented Understudy for Gisting Evaluation，具体用到的指标为ROUGE_L，即：Longest Common Subsequence ([LCS](https://en.wikipedia.org/wiki/Longest_common_subsequence_problem)) based statistics。
+
+测评函数详见`qichedashi.py`
+
+## 代码使用
+
+请按照如下步骤完成模型验证。
+* 在K-Lab内打开创建的比赛项目
+
+* 在Code Cell内执行下面的指令：
+```
+%%bash
+cd /home/kesci/work
+git clone https://github.com/Kesci/qichedashi_evaluation.git
+```
+* 完成仓库的clone之后，请在Code Cell内执行下面的命令，完成validation。
+```
+%%bash
+cd /home/kesci/work/qichedashi_evaluation/
+python3 qichedashi.py /path/to/submit/file /path/to/validation/file
+```
---
+++ qichedashi.py
@@ -0,0 +1,122 @@
+import sys
+import pandas as pd
+import numpy as np
+
+def normalize(s):
+    """
+    Normalize strings to space joined chars.
+
+    Args:
+        s: a list of strings.
+
+    Returns:
+        A list of normalized strings.
+    """
+    if not s:
+        return s
+    normalized = []
+    for ss in s:
+        tokens = [c for c in list(ss) if len(c.strip()) != 0]
+        normalized.append(' '.join(tokens))
+    return normalized
+
+def my_lcs(string, sub):
+    if len(string) < len(sub):
+        sub, string = string, sub
+    lengths = [[0 for i in range(0,len(sub)+1)] for j in range(0,len(string)+1)]
+
+    for j in range(1,len(sub)+1):
+        for i in range(1,len(string)+1):
+            if(string[i-1] == sub[j-1]):
+                lengths[i][j] = lengths[i-1][j-1] + 1
+            else:
+                lengths[i][j] = max(lengths[i-1][j] , lengths[i][j-1])
+
+    return lengths[len(string)][len(sub)]
+
+
+def calc_score(candidate, refs):
+
+    beta = 1.2
+
+    candidate = normalize(candidate)
+    refs = normalize(refs)
+
+    assert(len(candidate)==1)
+    assert(len(refs)==1)
+
+    prec = []
+    rec = []
+
+    token_c = candidate[0].split(" ")
+
+    for reference in refs:
+        token_r = reference.split(" ")
+        lcs = my_lcs(token_r, token_c)
+        prec.append(lcs/float(len(token_c)))
+        rec.append(lcs/float(len(token_r)))
+
+    prec_max = max(prec)
+    rec_max = max(rec)
+
+    if (prec_max!=0 and rec_max!=0):
+        score = ((1 + beta**2)*prec_max*rec_max)/float(rec_max + beta**2*prec_max)
+    else:
+        score = 0.0
+
+    return score
+
+
+def validate(submit_file, test_file):
+
+    submission = pd.read_csv(submit_file, dtype={'QID': str, 'Report': str})
+
+    groundtruth = pd.read_csv(test_file, dtype={'QID': str, 'Report': str})
+
+    if len(submission['QID'].unique()) != len(groundtruth['QID'].index):
+        raise Exception('预测结果中没有全部答案ID')
+
+    if len(np.intersect1d(submission['QID'], groundtruth['QID'])) <= 0:
+        raise Exception('No ID matches')
+    
+    if sorted(submission['QID'].unique()) != sorted(groundtruth['QID'].unique()):
+        raise Exception('Loss of submission IDs')
+        
+    return {
+        "code": 0,
+        "message": 'Validation Success',
+    }
+
+
+def score(submit_file, test_file):
+    submission = pd.read_csv(submit_file, dtype={'QID': str, 'Report': str}, index_col=False)
+
+    groundtruth = pd.read_csv(test_file, dtype={'QID': str, 'Report': str}, index_col=False)
+
+    tmp = pd.merge(submission, groundtruth, on='QID', how='inner')
+    tmp.columns = ['submit_answer', 'QID', 'truth_answer']
+
+    score = []
+
+    for submit, answer in zip(tmp['submit_answer'],tmp['truth_answer']):
+        sc = calc_score([submit],[answer])
+        score.append(sc)
+    
+    final_score = round(np.mean(score) * 100,4)
+
+    return {
+        "code": 0,
+        "score": final_score,
+        "message": "success"
+    }
+
+
+
+if __name__ == '__main__':
+    submit_file = sys.argv[1]
+    test_file =  sys.argv[2]
+    print(validate(submit_file, test_file))
+    print(score(submit_file, test_file))
+
+
+
